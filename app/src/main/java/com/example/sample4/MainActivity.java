21a8c25b632b823ca4a016561e626cd06496d5a7
wnsgk42 <57088799+wnsgk42@users.noreply.github.com>
2020-02-24 23:52:07 +0900

    finish

--- app/src/main/java/com/example/sample4/MainActivity.java
+++ app/src/main/java/com/example/sample4/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.sample4;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pager=findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+
+        Fragment1 fragment1 = new Fragment1();
+        Fragment2 fragment2 = new Fragment2();
+        Fragment3 fragment3 = new Fragment3();
+
+        myPagerAdapter.additem(fragment1);
+        myPagerAdapter.additem(fragment2);
+        myPagerAdapter.additem(fragment3);
+
+        pager.setAdapter(myPagerAdapter);
+
+        Button btn_2 = findViewById(R.id.btn_2);
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pager.setCurrentItem(1);
+            }
+        });
+    }
+}
+
+class MyPagerAdapter extends FragmentStatePagerAdapter{
+    ArrayList<Fragment> items = new ArrayList<>();
+    MyPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    public void additem (Fragment fragment) {
+        items.add(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "page"+position;
     }
 }
